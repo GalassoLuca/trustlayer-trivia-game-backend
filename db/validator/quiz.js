15ce175234845976c970e7adfa0dc898058a1e99
Luca Galasso <luca.galasso@wonderflow.ai>
2021-10-26 11:01:53 +0200

    test(validator): fix quiz validator tests

--- db/validator/quiz.js
+++ db/validator/quiz.js
@@ -1,12 +1,12 @@
 import validateQuestion from './question'
 import validateAnswers from './answers'
 
-export default function validate(quiz) {
-  if (!quiz.name) {
+export default function validate({ name, questions = [] } = {}) {
+  if (!name) {
     throw new TypeError('quiz name is required')
   }
 
-  quiz.questions.forEach(q => {
+  questions.forEach(q => {
     validateQuestion(q.question)
     validateAnswers(q.answers)
   })
--- db/validator/quiz.test.js
+++ db/validator/quiz.test.js
@@ -1,16 +1,18 @@
 import test from 'ava'
 import validateQuiz from './quiz'
 
-test('throw if the question is missing', t => {
+test('throw if the quiz name is missing', t => {
   const error = t.throws(() => validateQuiz())
 
   t.is(error.name, 'TypeError')
-  t.is(error.message, 'question is required')
+  t.is(error.message, 'quiz name is required')
 })
 
-test('throw if the answers are missing', t => {
-  const error = t.throws(() => validateQuiz({ question: 'question?' }))
+test('throw if the question is missing', t => {
+  const error = t.throws(() =>
+    validateQuiz({ name: 'Trivia', questions: [{}] })
+  )
 
   t.is(error.name, 'TypeError')
-  t.is(error.message, 'answer must be four')
+  t.is(error.message, 'question is required')
 })
