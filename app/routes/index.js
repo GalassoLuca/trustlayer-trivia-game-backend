9fefd4ec7ccf0ff2854225a80d06258576ed2237
Luca Galasso <luca.galasso@wonderflow.ai>
2021-10-27 16:07:47 +0200

    refactor(routes): use a single file for quiz route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,7 @@
-import getQuizzes from './quizzes.get'
-import getQuiz from './quiz.get'
-import postQuizzes from './quizzes.post'
-import putQuiz from './quiz.put'
-import deleteQuiz from './quiz.delete'
+import quizRoutes from './quiz'
 
 export async function routes(fastify, options) {
-  fastify.route(getQuizzes)
-  fastify.route(getQuiz)
-  fastify.route(postQuizzes)
-  fastify.route(putQuiz)
-  fastify.route(deleteQuiz)
+  quizRoutes.forEach(route => {
+    fastify.route(route)
+  })
 }
