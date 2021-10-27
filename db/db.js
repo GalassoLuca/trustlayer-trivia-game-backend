5851cc6532d6a0c02e40bae3f2fe27cad62729dc
Luca Galasso <luca.galasso@wonderflow.ai>
2021-10-27 16:17:08 +0200

    fix(db): use test env while testing

--- db/db.js
+++ db/db.js
@@ -1,11 +1,16 @@
 import { MongoClient } from 'mongodb'
+import { createLog } from '../log'
+
+const log = createLog('db')
 
 const url = 'mongodb://localhost:27017'
 const client = new MongoClient(url)
 
 await client.connect()
 
-const db = client.db('trivia-game')
+const selectedDB = `trivia-game-${process.env.NODE_ENV || 'development'}`
+log.info('Selected db: %s', selectedDB)
+const db = client.db(selectedDB)
 
 export { ObjectId } from 'mongodb'
 export const Quizzes = db.collection('quizzes')
